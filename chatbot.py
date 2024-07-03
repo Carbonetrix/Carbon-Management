2185bd9c7f395ba66b5c5ff3b4ce5061cf2f87c0
Usman3721 <olamidehassan007@gmail.com>
2024-07-03 09:43:26 +0100

    Putting Submit button,size of logo and making user question appaer immediately after been typed

--- chatbot.py
+++ chatbot.py
@@ -27,29 +27,16 @@ import base64
 from io import BytesIO
 import anthropic
 
-
+# model = genai.GenerativeModel("gemini-1.5-flash-latest")
+# chat = model.start_chat(history=[])
 
 
 # MongoDB Management
+# client = MongoClient(os.getenv("uri"))
 client = MongoClient(st.secrets.uri)
 db = client.get_database("Carbonetrix")
 users_collection = db['Carbonetrix']
 feedback_collection = db['Feedback'] 
-
-
-
-
-# print(genai.configure(api_key=os.getenv("GOOGLE_API_KEY")))
-
-
-
-# Initialize the generative model
-# model = genai.GenerativeModel("gemini-pro")
-# chat = model.start_chat(history=[])
-
-# model = genai.GenerativeModel("gemini-1.5-flash-latest")
-# chat = model.start_chat(history=[])
-
 
 
 
@@ -83,21 +70,42 @@ def add_question_to_db(question, timestamp):
         "timestamp": timestamp
     })
 
-def add_feedback_to_db(feedback, timestamp, question):
+def add_feedback_to_db(feedback, timestamp):
     feedback_collection.insert_one({
         "feedback": feedback, 
         "timestamp": timestamp,
         "company": st.session_state['company_name'], 
         "email": st.session_state['user_email'],
-        # "question": question
+    
     })
 
 
 
 # Prompting of the model through API
+# def get_gemini_response(question, level):
+#     level_instructions = {
+#         "Beginner": "Explain in simple terms with basic details.",
+#         "Intermediate": "Provide more detailed explanations with some technical terms.",
+#         "Professional": "Include advanced technical details and professional formulas and point of view."
+#     }
+
+#     prompt = (
+#         f"You are an AI Assistant specializing in Carbon Management for the Built Environment (construction, buildings, and infrastructure). "
+#         f"You are used by professionals in the field and construction organizations. Be explicit, informative, and explain technical concepts in a clear manner. "
+#         f"If any question apart from Carbon Management for the Built Environment or construction or infrastructure is asked, kindly say 'I'm sorry, I answer carbon-related questions only.' "
+#         f"Always provide at least 5 references and footnotes from reputable and accessible sources such as academic journals, official government or organizational websites, and well-known industry publications. "
+#         f"Make sure the references are accessible and not broken links. "
+#         f"Ensure the response is well-documented and avoid unnecessary breaking of lines. "
+#         f"{level_instructions[level]} "
+#         f"Question: {question}"
+#     )
+#     response = chat.send_message(prompt, stream=True)
+#     return response
+
+
 def get_claude_response(user_content, level):
-    # api_key = os.getenv("CARBONETRIX_ANTHROPIC_API_KEY")
     api_key = st.secrets.CARBONETRIX_ANTHROPIC_API_KEY
+    # api_key = os.getenv("CARBONETRIX_ANTHROPIC_API_KEY")
     client = anthropic.Anthropic(api_key=api_key)
     
     level_instructions = {
@@ -133,14 +141,17 @@ def main():
         st.sidebar.header("User Information")
         company_name = st.sidebar.text_input("Please enter your Username/company's name to continue:")
         user_email = st.sidebar.text_input("Please enter your email to continue:")
+      
 
-        if company_name and user_email:
-            st.session_state['company_name'] = company_name
-            st.session_state['user_email'] = user_email
-            st.experimental_rerun()
+        if st.sidebar.button("Submit"):
+            if company_name and user_email:
+                st.session_state['company_name'] = company_name
+                st.session_state['user_email'] = user_email
+                st.experimental_rerun()
+            else:
+                st.sidebar.warning("Please enter both your company name and email to continue.")
         else:
             st.stop()
-
 
     with st.sidebar:
         st.header("Feedback")
@@ -149,7 +160,7 @@ def main():
             timestamp = datetime.now()
             add_feedback_to_db(feedback, timestamp)
             st.success("Thank you for your feedback!")
-   
+
     # st.set_page_config(layout="wide")
     c1, c2, c3 = st.columns([1, 2, 1])
     pth = "Images/logo.png"
@@ -169,12 +180,12 @@ def main():
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
     user_avatar_style="identicon"
-    user_seed = "1234"
+    user_seed = "123456"
 
     def image_to_base64(image_path):
         img = Image.open(image_path)
         buffer = BytesIO()
-        img.save(buffer, format="PNG")
+        img.save(buffer, format="PNG",)
         img_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
         return img_str
 
@@ -191,31 +202,37 @@ def main():
         if st.session_state.messages:
             for i, msg in enumerate(st.session_state.messages):
                 if isinstance(msg, dict) and msg.get("role") == "user" and "content" in msg:
-                    message(msg["content"], is_user=True, key=f"user_{i}",avatar_style=user_avatar_style, seed=user_seed,)
+                    # st.chat_message(msg["content"], is_user=True, key=f"user_{i}")
+                    message(msg["content"], is_user=True, key=f"user{i}",avatar_style=user_avatar_style, seed=user_seed,)
                 elif isinstance(msg, dict) and msg.get("role") == "assistant" and "content" in msg:
                     # Custom HTML for displaying the company logo
                     st.markdown(f"""
                     <div id='{i}' style='text-align: left; color: black; background-color: #f4f4f4; padding: 10px; border-radius: 10px; margin: 10px 0;'>
-                        <img src='{bot_logo_html}' alt='Bot Avatar' style='width: 50px; height: 50px; border-radius: 50%; display: inline-block; vertical-align: middle; margin-right: 10px;' />
+                        <img src='{bot_logo_html}' alt='Bot Avatar' style='width: 30px; height: 30px; border-radius: 50%; display: inline-block; vertical-align: middle; margin-right: 10px;' />
                         <span style='vertical-align: middle;'>\n{msg['content']}\n</span>
                     </div>
                     """, unsafe_allow_html=True)
                 elif not isinstance(msg, dict) or "role" not in msg or "content" not in msg:
                     st.error("Invalid message format")
-  
 
 
 
 
-
+    # with input_container:
     if prompt := st.chat_input():
+        message(prompt, is_user=True, key=f"user{i}",avatar_style=user_avatar_style, seed=user_seed,)
         timestamp = datetime.now()
         add_question_to_db(prompt, timestamp)
+      
+        
         with input_container:
             if prompt:
-                response_text = get_claude_response(prompt, st.session_state['level'])
-                # response_text = "".join(chunk.text for chunk in response)
                 
+                response_text = get_claude_response(prompt, st.session_state['level'])
+                # response = get_gemini_response(prompt, st.session_state['level'])
+                # response_text = "".join(chunk.text for chunk in response)
+        
+        
 
                 timestamp = datetime.now()
                 st.session_state['chat_history'].append(("You", prompt, timestamp))
